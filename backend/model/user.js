6b335fb6271cc40c6f1c4b57b1ee9bb7cdd93769
ayushhub-hub <ayushsable87@gmail.com>
2025-01-24 11:46:25 +0530

    milestone 2,5,6,7,8 done

--- backend/model/user.js
+++ backend/model/user.js
@@ -79,37 +79,37 @@ const userSchema = new mongoose.Schema({
 
 //Hash password
 
-userSchema.pre("save",async function(next){
-    if(!this.isModified("password")){
-        next();
-    }
+// userSchema.pre("save",async function(next){
+//     if(!this.isModified("password")){
+//         next();
+//     }
 
-    this.password = await bcrypt.hash(this.password,10)
-})
-
-
-
-// jsonwebtoken
+//     this.password = await bcrypt.hash(this.password,10)
+// })
 
 
 
-userSchema.methods.getJwtToken = function(){
-    return jwt.sign({id:this_id},process.env.JWT_SECRET_KEY,{
-        expiresIn:process.env.JWT_EXPIRES,
-    })
-}
-
-
-//compare password
-
-
-userSchema.methods.comparePassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword,this.password)
+// // jsonwebtoken
 
 
 
+// userSchema.methods.getJwtToken = function(){
+//     return jwt.sign({id:this_id},process.env.JWT_SECRET_KEY,{
+//         expiresIn:process.env.JWT_EXPIRES,
+//     })
+// }
 
-}
+
+// //compare password
+
+
+// userSchema.methods.comparePassword = async function(enteredPassword){
+//     return await bcrypt.compare(enteredPassword,this.password)
+
+
+
+
+// }
 
 
 
