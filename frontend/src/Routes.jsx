13653e8ddb3ab11a33c2ace1acf090b66278ec1b
ayushhub-hub <ayushsable87@gmail.com>
2025-01-28 13:27:04 +0530

    M9 done

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,5 @@
 import LoginPage from './pages/Login'
-import SignupPage from "./pages/Signup";
+import SignupPage from './pages/Signup';
 import Home from './pages/Home';
-
-export {LoginPage,SignupPage,Home};
+import CreateProduct from './pages/createProduct';
+export {LoginPage,SignupPage,Home,CreateProduct};
