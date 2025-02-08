c05227119cce5fbef7f645a1674d54fe2c6f2293
ayushhub-hub <ayushsable87@gmail.com>
2025-02-08 10:14:53 +0530

    M14 done

--- frontend/src/components/auth/myProduct.jsx
+++ frontend/src/components/auth/myProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types"
 import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 const Myproduct=({_id,name,images,description,price})=>{
     const[currentIndex,setCurrentIndex]=useState(0)
@@ -17,17 +18,30 @@ const Myproduct=({_id,name,images,description,price})=>{
     const handleEdit=()=>{
         navigate(`/create-product/${_id}`)
     }
+    const handleDelete=async()=>{
+        try{
+            const response=await axios.delete(`http://localhost:8000/api/v2/product/delete-product/${_id}`)
+            if(response.status===200){
+                alert("Product Deleted!")
+                window.location.reload()
+            }
+            }catch(err){
+                console.error(`Could not delete product${err}`)
+                alert("Failed to delete!")
+            }
+        }
 
-    const currentImage = images && images.length > 0 ? images[currentIndex] : null
-
-    return (
-        <div className="bg-neutral-200 p-4 rounded-lg shadow-md flex flex-col justify-between">
+        
+        const currentImage = images && images.length > 0 ? images[currentIndex] : null
+        
+        return (
+            <div className="bg-neutral-200 p-4 rounded-lg shadow-md flex flex-col justify-between">
             <div className="w-full">
                 {currentImage && (
                     <img
-                        src={`http://localhost:8000${currentImage}`}
-                        alt={name}
-                        className="w-full h-56 object-cover rounded-lg mb-2"
+                    src={`http://localhost:8000${currentImage}`}
+                    alt={name}
+                    className="w-full h-56 object-cover rounded-lg mb-2"
                     />
                 )}
                 <h2 className="text-lg font-bold">{name}</h2>
@@ -38,13 +52,18 @@ const Myproduct=({_id,name,images,description,price})=>{
                 <button
                     className="w-full text-white px-4 py-2 rounded-md bg-neutral-900 hover:bg-neutral-700 transition duration-300"
                     onClick={handleEdit}
-                >
+                    >
                     Edit
                 </button>
+                <button className="w-full text-white px-4 py-2 rounded-md bg-red-500 hover:bg-red-400 transition duration-300" onClick={handleDelete}>
+                    Delete
+                </button>
+
             </div>
         </div>
     );
 }
+
 
 Myproduct.propTypes={
     _id:PropTypes.string.isRequired,
