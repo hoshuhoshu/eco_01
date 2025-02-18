d37c4d617e31b7b65c0338ef810ac79e38e74b7c
ayushhub-hub <ayushsable87@gmail.com>
2025-02-18 15:55:33 +0530

    milestone15 is done

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,12 +1,7 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import CreateProduct from "./pages/createProduct";
-import MyProducts from "./pages/myproduct";
-
-
-export { LoginPage };
-export{SignupPage};
-export{Home};
-export{CreateProduct};
-export{MyProducts}
+import MyProducts from './pages/myproduct';
+import Cart from './pages//cart';
+export { LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart };
